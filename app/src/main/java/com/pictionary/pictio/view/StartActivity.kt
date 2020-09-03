90a3adcf6f46fd13b4c807234f193b4241ca92cf
Adityaraj1711 <adityamailbox0@gmail.com>
2020-09-03 06:37:13 +0530

    added basic activities and craeted login and register layout

--- app/src/main/java/com/pictionary/pictio/view/StartActivity.kt
+++ app/src/main/java/com/pictionary/pictio/view/StartActivity.kt
@@ -1,11 +1,12 @@
-package com.pictionary.pictio
+package com.pictionary.pictio.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.pictionary.pictio.R
 
 class StartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_start)
     }
 }
