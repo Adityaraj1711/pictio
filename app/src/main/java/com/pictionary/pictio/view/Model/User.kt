7869fc2600185733fb5da1b4785003a56209a772
Adityaraj1711 <adityamailbox0@gmail.com>
2020-09-03 19:33:12 +0530

    created User model so that one could search the user from search fragment

---
+++ app/src/main/java/com/pictionary/pictio/view/Model/User.kt
@@ -0,0 +1,9 @@
+package com.pictionary.pictio.view.Model
+
+data class User(
+    val id: String,
+    val username: String,
+    val fullname: String,
+    val imageurl: String,
+    val bio: String
+)
